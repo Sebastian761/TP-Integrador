2446f9c689012d0bcbeed28ec8d311820d807173
Sebastian Ocando <ocandovivas@gmail.com>
2022-10-27 17:16:24 -0300

    Version funcional

---
+++ js/compra_tickets.js
@@ -0,0 +1,46 @@
+
+const precioTicket = 200;
+
+function resumen(event){
+    event.preventDefault();
+    let cantidad = document.getElementById('cantidad');
+    let categoria = document.getElementById("seleccion");
+    let input = document.getElementById('total');
+    let value = categoria.options[categoria.selectedIndex].value;
+
+    let stringEstudiante = 'Estudiante';
+    let stringTrainee = 'Trainee';
+    let stringJunior = 'Junior';
+    
+    let valorTicketEstudiante = precioTicket - (200 * 80) / 100;
+    let valorTicketTrainee = precioTicket - (200 * 50) / 100;
+    let valorTicketJunior = precioTicket - (200 * 15) / 100;
+
+    if(value == stringEstudiante){
+        let pago = valorTicketEstudiante * cantidad.value;
+        input.placeholder = 'Total a pagar: $ ' + pago;
+    }
+    else if(value == stringTrainee){
+        let pago = valorTicketTrainee * cantidad.value;
+        input.placeholder = 'Total a pagar: $ ' + pago;
+    }
+    else if(value == stringJunior){
+        
+        let pago = valorTicketJunior * cantidad.value;
+        input.placeholder = 'Total a pagar: $ ' + pago;
+    }
+}
+
+function borrar(){
+    document.getElementById('nombre').value = '';
+    document.getElementById('apellido').value = '';
+    document.getElementById('correo').value = '';
+    document.getElementById('cantidad').value = '';
+    document.getElementById('seleccion').value = '';
+}
+
+botonResumen = document.getElementById('resumen');
+botonResumen.addEventListener('click', resumen);
+
+botonBorrar = document.getElementById('borrar');
+botonBorrar.addEventListener('click', borrar);
